318bf49b407b3add22b3ee5566a17ea449ab12b1
chriscitric <chriscitric@gmail.com>
2020-08-08 16:12:50 -0500

    add bootstrap

--- client/request.js
+++ client/request.js
@@ -2,6 +2,7 @@ var app = document.getElementById("app");
 console.log(app);
 const request = new XMLHttpRequest();
 const url = "https://ghibliapi.herokuapp.com/films";
+var cards = [];
 
 request.open("get",url,true);
 request.send();
@@ -14,8 +15,11 @@ movies.forEach((movie)=>{
 console.log(movie.title);
 
 const card = document.createElement("div")
+card.setAttribute("class","card my-3")
 const title = document.createElement("h2")
+title.setAttribute("class","card-header bg-primary text-light")
 const description = document.createElement("p")
+description.setAttribute("class","card-body")
 
 title.textContent = movie.title;
 description.textContent = movie.description;
@@ -23,6 +27,12 @@ description.textContent = movie.description;
 app.appendChild(card);
 card.appendChild(title);
 card.appendChild(description);
+cards.push(card);
 })
 
+}
+
+
+function sort(){
+    
 }
